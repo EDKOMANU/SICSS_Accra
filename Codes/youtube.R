c47aa296d9c6be4959f7164ff0736269048d265a
EDKOMANU <manuedward34@gmail.com>
2024-11-09 08:31:47 +0000

    Update youtube.R

--- Codes/youtube.R
+++ Codes/youtube.R
@@ -7,13 +7,15 @@ library(vosonSML)
 
 options(stringsAsFactors = FALSE) # setting strings as characters so that they appear as they were in the dataset
 
-youtube_auth <- Authenticate("youtube", apiKey = "AIzaSyBIZcKqKB47tlZr9DkkBUvuRw4AzV-HAc0") #Activating your youtube API
+youtube_auth <- Authenticate("youtube", apiKey = "AIzaSyBIZcKqKB47tlZr9DkkBUvuRw4AzV-HAc0", ) #Activating your youtube API
 
-video_ids = c("71hNxnjAfn0") #Video ID
+galampsey = c("71hNxnjAfn0", "frJhTkX3E6M", "kj2S5RcfvKc", "1b4C3ORyDR4", "WGhU8s2wp_Q","fCulR9-6AV8", "Yuy-ScBotvA",
+              "u7oOUwroT0o", "Pq3QkXD_g18","n87dumuGbI", "caGclk8yEHk", 
+              "S4hwWtRlNqQ") #Video ID
 comments <- youtube_auth  |> # Collecting the comments from the video
-  Collect(videoIDs = video_ids,
+  Collect(videoIDs = galampsey,
           maxComments = 4300,
-          verbose = FALSE)
+          verbose = T)
 
 Both_ids = c("-sPcTO2mv6o&t=2s", "R6VKnOXyITk")
 Both_comments <- youtube_auth  |> 
